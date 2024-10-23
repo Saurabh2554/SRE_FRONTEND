035f989201555a2807fdb0776d32422c81575fa2
shridhar-dax <147593402+shridhar-dax@users.noreply.github.com>
2024-10-24 00:16:20 +0530

    adding auth to headers (#4)
    * adding auth to headers
    
    * solve header issues

--- frontend/src/common/components/Snackbar/Snackbar.jsx
+++ frontend/src/common/components/Snackbar/Snackbar.jsx
@@ -5,7 +5,7 @@ import Alert from "@mui/material/Alert";
 export const ReusableSnackbar = ({open, handleClose, message, severity}) => {
     return(
         <>
-            <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}>
+            <Snackbar open={open} autoHideDuration={5000} onClose={handleClose} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} key="top-right">
                 <Alert onClose={handleClose} severity={severity} sx={{ width: '100%' }}>
                     {message}
                 </Alert>
